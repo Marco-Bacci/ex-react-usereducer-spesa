29727ca3834ac9cbc18457bcf6813ccd2b0e5a3a
Marco Bacci <marcobacci94@gmail.com>
2025-12-01 13:22:07 +0100

    milestone 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   const products = [
     { name: "Mela", price: 0.5 },
@@ -6,14 +8,50 @@ function App() {
     { name: "Pasta", price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts);
+
+  function addToCart(product) {
+    const alreadyInCart = addedProducts.some((p) => p.name === product.name);
+
+    if (alreadyInCart) {
+      return;
+    }
+
+    const productToAdd = {
+      ...product,
+      quantity: 1,
+    };
+
+    setAddedProducts((curr) => [...curr, productToAdd]);
+  }
+
   return (
-    <ul>
-      {products.map((product, i) => (
-        <li key={i}>
-          {product.name} - {product.price}€
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>PRODOTTI</h2>
+      <ul>
+        {products.map((product, i) => (
+          <li key={i}>
+            {product.name} - {product.price.toFixed(2)}€
+            <button onClick={() => addToCart(product)}>
+              Aggiungi al carrello
+            </button>
+          </li>
+        ))}
+      </ul>
+      {addedProducts.length > 0 && (
+        <>
+          <h3>Prodotti nel carrello</h3>
+          <ul>
+            {addedProducts.map((product, i) => (
+              <li key={i}>
+                {product.quantity} x {product.name} {product.price.toFixed(2)}€
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
   );
 }
 
